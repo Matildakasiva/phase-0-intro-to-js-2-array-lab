1e071ed0914cf87c5807db85ae668893dadd1bdd
Matilda <ogarimatilda@gmail.com>
2024-03-28 08:33:47 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+console.log(cats)
+
+
+
+ function destructivelyAppendCat(Ralph){
+    cats.push('Ralph')
+ }
+
+ function destructivelyPrependCat(Bob){
+    cats.unshift('Bob')
+ }
+  
+ function destructivelyRemoveLastCat(){
+    cats.pop()
+ }
+
+ function destructivelyRemoveFirstCat(){
+    cats.shift()
+ }
+
+ function appendCat(Broom){
+    let newArray = [...cats, Broom]
+    return newArray
+ }
+
+ function prependCat(Arnold){
+    let newArray = [Arnold, ...cats]
+    return newArray
+ }
+
+ function removeLastCat(){
+    let newArray = cats.slice(0, -1)
+    return newArray
+ }
+
+ function removeFirstCat(){
+    let newArray = cats.slice(1)
+    return newArray
+ }
